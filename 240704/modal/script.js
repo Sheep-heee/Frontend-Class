4328d7f527f1ff496eb94fd006363cb3a0b70d86
Sheep-heee <sheephi0609@gmail.com>
2024-07-04 18:20:11 +0900

    Add files via upload

---
+++ 240704/modal/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("#open");
+const modalBox = document.querySelector("#modal_box");
+const close = document.querySelector("#close");
+btn.addEventListener("click", () => {
+  modalBox.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  modalBox.classList.remove("active");
+});
+
+modalBox.addEventListener("click", function () {
+  this.classList.remove("active");
+});
