295515768d281779200a8e0e121a3b2a9352b649
sheep-heee <sheephi0609@gmail.com>
2024-12-16 16:23:44 +0900

    241216

--- 241120/src/controllers/userController.js
+++ 241120/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { model } from "mongoose";
 import User from "../models/user";
 import bcrypt from "bcrypt";
 
@@ -230,8 +231,14 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
-  console.log(user);
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
+
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not Found." });
   }
